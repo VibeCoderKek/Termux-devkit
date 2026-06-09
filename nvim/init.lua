42ef4163045c7e3ffcf07af4625983fd1536042b
VibeCoderKek <VibeCoderKek@users.noreply.github.com>
2026-06-09 11:11:59 -0700

    chore: checkpoint before roadmap implementation

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,14 +26,14 @@ vim.g.mapleader = " "
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
@@ -43,162 +43,162 @@ vim.opt.rtp:prepend(lazypath)
 -- =========================================================
 
 require("lazy").setup({
-  spec = {
-    { "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
+	spec = {
+		{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
 
-    {
-      "nvim-telescope/telescope.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-    },
+		{
+			"nvim-telescope/telescope.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+		},
 
-    {
-      "nvim-tree/nvim-tree.lua",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+		{
+			"nvim-tree/nvim-tree.lua",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+		},
 
-    { "lewis6991/gitsigns.nvim" },
+		{ "lewis6991/gitsigns.nvim" },
 
-    {
-      "neovim/nvim-lspconfig",
-      config = function()
-        require("lspconfig")
-      end,
-    },
+		{
+			"neovim/nvim-lspconfig",
+			config = function()
+				require("lspconfig")
+			end,
+		},
 
-    { "hrsh7th/nvim-cmp" },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "L3MON4D3/LuaSnip" },
-    { "saadparwaiz1/cmp_luasnip" },
-    {
-      "rafamadriz/friendly-snippets",
-      config = function()
-        require("luasnip.loaders.from_vscode").lazy_load()
-      end,
-    },
-    {
-      "folke/tokyonight.nvim",
-      priority = 1000,
-      config = function()
-        require("tokyonight").setup({
-          style = "night",
-          transparent = false,
-          terminal_colors = true,
-        })
-        vim.cmd("colorscheme tokyonight")
-      end,
-    },
-    {
-      "nvim-lualine/lualine.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      config = function()
-        require("lualine").setup({
-          options = {
-            theme = "tokyonight",
-            component_separators = { left = "", right = "" },
-            section_separators = { left = "", right = "" },
-          },
-          sections = {
-            lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
-            lualine_c = { { "filename", path = 1 } },
-            lualine_x = { "encoding", "filetype" },
-            lualine_y = { "progress" },
-            lualine_z = { "location" },
-          },
-        })
-      end,
-    },
-    {
-      "folke/which-key.nvim",
-      event = "VeryLazy",
-      config = function()
-        local wk = require("which-key")
-        wk.setup({
-          delay = 400,
-          icons = { rules = false },
-        })
-        wk.add({
-          { "<leader>e",  desc = "Toggle file tree" },
-          { "<leader>f",  desc = "Find files" },
-          { "<leader>g",  desc = "Live grep" },
-          { "<leader>b",  desc = "Buffers" },
-          { "<leader>w",  desc = "Save file" },
-          { "<leader>q",  desc = "Quit" },
-          { "<leader>h",  desc = "Clear search highlight" },
-          { "<leader>rn", desc = "LSP rename" },
-          { "<leader>gg", desc = "Lazygit" },
-          { "<leader>xx", desc = "Trouble diagnostics" },
-          { "gd",         desc = "LSP go to definition" },
-          { "K",          desc = "LSP hover" },
-        })
-      end,
-    },
-    {
-      "kdheepak/lazygit.nvim",
-      dependencies = { "nvim-lua/plenary.nvim" },
-      config = function()
-        vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "Lazygit" })
-      end,
-    },
-    {
-      "windwp/nvim-autopairs",
-      event = "InsertEnter",
-      config = function()
-        local autopairs = require("nvim-autopairs")
-        autopairs.setup({ check_ts = true })
-        local cmp_autopairs = require("nvim-autopairs.completion.cmp")
-        local cmp = require("cmp")
-        cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
-      end,
-    },
-    {
-      "numToStr/Comment.nvim",
-      config = function()
-        require("Comment").setup()
-      end,
-    },
-    {
-      "stevearc/conform.nvim",
-      config = function()
-        require("conform").setup({
-          formatters_by_ft = {
-            javascript  = { "prettier" },
-            typescript  = { "prettier" },
-            html        = { "prettier" },
-            css         = { "prettier" },
-            json        = { "prettier" },
-            python      = { "black" },
-            sh          = { "shfmt" },
-            lua         = { "stylua" },
-          },
-          format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-          },
-        })
-      end,
-    },
-    {
-      "folke/trouble.nvim",
-      dependencies = { "nvim-tree/nvim-web-devicons" },
-      config = function()
-        require("trouble").setup()
-        vim.keymap.set("n", "<leader>xx", ":Trouble diagnostics toggle<CR>", { desc = "Trouble diagnostics" })
-      end,
-    },
-    {
-      "lukas-reineke/indent-blankline.nvim",
-      main = "ibl",
-      config = function()
-        require("ibl").setup({
-          indent = { char = "│" },
-          scope  = { enabled = true },
-        })
-      end,
-    },
-  },
+		{ "hrsh7th/nvim-cmp" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "L3MON4D3/LuaSnip" },
+		{ "saadparwaiz1/cmp_luasnip" },
+		{
+			"rafamadriz/friendly-snippets",
+			config = function()
+				require("luasnip.loaders.from_vscode").lazy_load()
+			end,
+		},
+		{
+			"folke/tokyonight.nvim",
+			priority = 1000,
+			config = function()
+				require("tokyonight").setup({
+					style = "night",
+					transparent = false,
+					terminal_colors = true,
+				})
+				vim.cmd("colorscheme tokyonight")
+			end,
+		},
+		{
+			"nvim-lualine/lualine.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("lualine").setup({
+					options = {
+						theme = "tokyonight",
+						component_separators = { left = "", right = "" },
+						section_separators = { left = "", right = "" },
+					},
+					sections = {
+						lualine_a = { "mode" },
+						lualine_b = { "branch", "diff", "diagnostics" },
+						lualine_c = { { "filename", path = 1 } },
+						lualine_x = { "encoding", "filetype" },
+						lualine_y = { "progress" },
+						lualine_z = { "location" },
+					},
+				})
+			end,
+		},
+		{
+			"folke/which-key.nvim",
+			event = "VeryLazy",
+			config = function()
+				local wk = require("which-key")
+				wk.setup({
+					delay = 400,
+					icons = { rules = false },
+				})
+				wk.add({
+					{ "<leader>e", desc = "Toggle file tree" },
+					{ "<leader>f", desc = "Find files" },
+					{ "<leader>g", desc = "Live grep" },
+					{ "<leader>b", desc = "Buffers" },
+					{ "<leader>w", desc = "Save file" },
+					{ "<leader>q", desc = "Quit" },
+					{ "<leader>h", desc = "Clear search highlight" },
+					{ "<leader>rn", desc = "LSP rename" },
+					{ "<leader>gg", desc = "Lazygit" },
+					{ "<leader>xx", desc = "Trouble diagnostics" },
+					{ "gd", desc = "LSP go to definition" },
+					{ "K", desc = "LSP hover" },
+				})
+			end,
+		},
+		{
+			"kdheepak/lazygit.nvim",
+			dependencies = { "nvim-lua/plenary.nvim" },
+			config = function()
+				vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "Lazygit" })
+			end,
+		},
+		{
+			"windwp/nvim-autopairs",
+			event = "InsertEnter",
+			config = function()
+				local autopairs = require("nvim-autopairs")
+				autopairs.setup({ check_ts = true })
+				local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+				local cmp = require("cmp")
+				cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+			end,
+		},
+		{
+			"numToStr/Comment.nvim",
+			config = function()
+				require("Comment").setup()
+			end,
+		},
+		{
+			"stevearc/conform.nvim",
+			config = function()
+				require("conform").setup({
+					formatters_by_ft = {
+						javascript = { "prettier" },
+						typescript = { "prettier" },
+						html = { "prettier" },
+						css = { "prettier" },
+						json = { "prettier" },
+						python = { "black" },
+						sh = { "shfmt" },
+						lua = { "stylua" },
+					},
+					format_on_save = {
+						timeout_ms = 500,
+						lsp_fallback = true,
+					},
+				})
+			end,
+		},
+		{
+			"folke/trouble.nvim",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				require("trouble").setup()
+				vim.keymap.set("n", "<leader>xx", ":Trouble diagnostics toggle<CR>", { desc = "Trouble diagnostics" })
+			end,
+		},
+		{
+			"lukas-reineke/indent-blankline.nvim",
+			main = "ibl",
+			config = function()
+				require("ibl").setup({
+					indent = { char = "│" },
+					scope = { enabled = true },
+				})
+			end,
+		},
+	},
 })
 
 -- =========================================================
@@ -206,8 +206,8 @@ require("lazy").setup({
 -- =========================================================
 
 require("nvim-tree").setup({
-  view = { width = 35 },
-  git = { enable = true },
+	view = { width = 18 },
+	git = { enable = true },
 })
 
 -- =========================================================
@@ -225,38 +225,38 @@ local cmp = require("cmp")
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 cmp.setup({
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { "i", "s" }),
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-  }),
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "luasnip" },
-    { name = "buffer" },
-    { name = "path" },
-  },
+	snippet = {
+		expand = function(args)
+			luasnip.lsp_expand(args.body)
+		end,
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_next_item()
+			elseif luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if cmp.visible() then
+				cmp.select_prev_item()
+			elseif luasnip.jumpable(-1) then
+				luasnip.jump(-1)
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<CR>"] = cmp.mapping.confirm({ select = true }),
+	}),
+	sources = {
+		{ name = "nvim_lsp" },
+		{ name = "luasnip" },
+		{ name = "buffer" },
+		{ name = "path" },
+	},
 })
 
 -- =========================================================
@@ -264,17 +264,17 @@ cmp.setup({
 -- =========================================================
 
 local servers = {
-  "pyright",
-  "ts_ls",
-  "html",
-  "cssls",
-  "bashls",
+	"pyright",
+	"ts_ls",
+	"html",
+	"cssls",
+	"bashls",
 }
 
 for _, server in ipairs(servers) do
-  vim.lsp.config(server, {
-    capabilities = capabilities,
-  })
+	vim.lsp.config(server, {
+		capabilities = capabilities,
+	})
 end
 
 vim.lsp.enable(servers)
@@ -305,4 +305,4 @@ vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
 -- PROJ WORKSPACE INTEGRATION
 -- =========================================================
 
-require('proj')
+require("proj")
--- nvim/lua/proj.lua
+++ nvim/lua/proj.lua
@@ -1,9 +1,6 @@
 -- =============================================================================
 -- lua/proj.lua  —  proj workspace integration for Neovim
 -- =============================================================================
--- Drop into your Neovim config at: ~/.config/nvim/lua/proj.lua
--- Then add to init.lua:  require('proj')
--- =============================================================================
 
 local M = {}
 
@@ -14,17 +11,13 @@ local run_dir     = vim.fn.expand("~/.proj/run")
 -- Helpers
 -- ---------------------------------------------------------------------------
 
--- Derive the workspace name from the current tmux session name
--- tmux session is named "proj-<name>"; strip the prefix.
 local function get_workspace_name()
-  -- Try env var first (set by proj script)
   local name = vim.env.PROJ_WORKSPACE
   if name and name ~= "" then return name end
 
-  -- Fall back to parsing the tmux session name
   local session = vim.fn.system("tmux display-message -p '#S' 2>/dev/null"):gsub("\n", "")
   if session:match("^proj%-") then
-    return session:sub(6)  -- strip "proj-"
+    return session:sub(6)
   end
   return nil
 end
@@ -36,8 +29,8 @@ end
 -- ---------------------------------------------------------------------------
 -- Session save
 -- ---------------------------------------------------------------------------
+
 function M.save_session()
-  -- Ensure session dir exists
   vim.fn.mkdir(session_dir, "p")
 
   local name = get_workspace_name()
@@ -45,11 +38,9 @@ function M.save_session()
 
   local sf = session_file(name)
 
-  -- Clean up before saving: close nvim-tree (it doesn't restore cleanly)
   local ok, nvim_tree = pcall(require, "nvim-tree.api")
   if ok then nvim_tree.tree.close() end
 
-  -- mksession
   vim.cmd("mksession! " .. vim.fn.fnameescape(sf))
   vim.notify("[proj] Session saved: " .. name, vim.log.levels.INFO)
 end
@@ -57,42 +48,33 @@ end
 -- ---------------------------------------------------------------------------
 -- Session restore
 -- ---------------------------------------------------------------------------
+
 function M.restore_session()
   local name = get_workspace_name()
   if not name then return end
 
   local sf = session_file(name)
   if vim.fn.filereadable(sf) == 1 then
-    -- Source session only if we were called with no file args
     if vim.fn.argc() == 0 then
       vim.cmd("silent! source " .. vim.fn.fnameescape(sf))
       vim.notify("[proj] Session restored: " .. name, vim.log.levels.INFO)
     end
-  else
-    -- First time: open nvim-tree
-    vim.schedule(function()
-      local ok, _ = pcall(vim.cmd, "NvimTreeOpen")
-      if not ok then
-        pcall(vim.cmd, "Neotree show")  -- fallback to neo-tree
-      end
-    end)
   end
 end
 
 -- ---------------------------------------------------------------------------
 -- Autocommands
 -- ---------------------------------------------------------------------------
+
 local function setup_autocmds()
   local group = vim.api.nvim_create_augroup("ProjWorkspace", { clear = true })
 
-  -- Save session on exit
   vim.api.nvim_create_autocmd("VimLeavePre", {
     group    = group,
     callback = M.save_session,
     desc     = "[proj] Auto-save nvim session on exit",
   })
 
-  -- Restore session on startup (deferred so plugins are loaded first)
   vim.api.nvim_create_autocmd("VimEnter", {
     group    = group,
     nested   = true,
@@ -101,7 +83,6 @@ local function setup_autocmds()
     desc     = "[proj] Auto-restore nvim session on enter",
   })
 
-  -- Update lock file with nvim PID so proj can track it
   vim.api.nvim_create_autocmd("VimEnter", {
     group    = group,
     once     = true,
@@ -111,7 +92,6 @@ local function setup_autocmds()
       local pid  = tostring(vim.fn.getpid())
       local lock = run_dir .. "/" .. name .. ".lock"
       if vim.fn.filereadable(lock) == 1 then
-        -- Update nvim_pid field in lock JSON via jq
         vim.fn.system(string.format(
           "jq --arg p %s '.nvim_pid = $p' %s > %s.tmp && mv %s.tmp %s",
           vim.fn.shellescape(pid),
@@ -129,19 +109,20 @@ end
 -- ---------------------------------------------------------------------------
 -- User commands
 -- ---------------------------------------------------------------------------
+
 local function setup_commands()
   vim.api.nvim_create_user_command("ProjSave",    M.save_session,    { desc = "Save proj workspace session" })
   vim.api.nvim_create_user_command("ProjRestore", M.restore_session, { desc = "Restore proj workspace session" })
 
   vim.api.nvim_create_user_command("ProjSwitch", function()
-    -- Drop to shell and run proj switch (which uses fzf)
     vim.cmd("terminal proj switch")
   end, { desc = "Switch proj workspace via fzf" })
 end
 
 -- ---------------------------------------------------------------------------
--- Setup entry point
+-- Setup
 -- ---------------------------------------------------------------------------
+
 function M.setup(opts)
   opts = opts or {}
   vim.fn.mkdir(session_dir, "p")
@@ -149,8 +130,6 @@ function M.setup(opts)
   setup_commands()
 end
 
--- Auto-setup when required
 M.setup()
 
 return M
-
